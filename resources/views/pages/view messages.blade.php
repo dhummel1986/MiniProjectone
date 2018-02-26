41173e68e31af4db4578297b4a8d1ba1285c0977
diana <dpa6@njit.edu>
2018-02-26 08:50:38 -0500

    7 tasks out 8 working, debugging view messages

---
+++ resources/views/pages/view messages.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.default')
+@section('content')
+
+    <h1>This is the logout Page</h1>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,47 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('pages.about');
 });
+
 Route::get('/contact', function () {
     return view('pages.contact');
 });
-Route::post('/', function () {
-    return view('welcome');
+
+Route::post('/contact', function () {
+
+    $data = request()->all();
+    echo "email: " . $data['email'] . '<br>';
+    echo "subject: " . $data['subject'] . '<br>';
+    echo "body: " . $data['body'];
+
+});
+Route::get('/SignIn', function () {
+    return view('pages.SignIn');
+});
+Route::post('/SignIn', function () {
+
+    $data = request()->all();
+    echo "username: " . $data['username'] . '<br>';
+    echo "password: " . $data['password'];
+});
+
+Route::get('/Signup', function () {
+    return view('pages.Signup');
+});
+
+    Route::post('/Signup', function () {
+
+        $data = request()->all();
+        echo "name: " . $data['name'] . '<br>';
+        echo "email: " . $data['email'] . '<br>';
+        echo "password: " . $data['password'] . '<br>';
+        echo "password: " . $data['password'];
+    });
+
+
+Route::get('/logout', function () {
+    return view('pages.logout');
+});
+
+Route::get('/view messages', function () {
+    return view('pages.view messages');
 });
